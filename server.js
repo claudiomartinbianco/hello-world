d29e488fba30e63b586a988a777b2a04e1796550
Claudio Martin Bianco <39170327+claudiomartinbianco@users.noreply.github.com>
2021-01-26 21:33:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,3 +5,13 @@ res.writeHead(200);
 res.end('Hi Whirlpool!');
 });
 server.listen(80);
+
+
+public deleteTrack(index: number) {
+ // out of bound check
+ if(!(index <= this.tracks.length)) {
+   return;
+ }
+
+ this.tracks.splice(index,1);
+}
